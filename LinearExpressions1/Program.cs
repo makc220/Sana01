77c3384a8e35e4fe2e0b47915f5c2a38bf0dc3e0
Максим Бондарчук <bondarcumk086@gmail.com>
2024-11-02 13:01:40 +0200

    Початковий стан проєкту

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.Write("Введіть значення для a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення для b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення для c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення для d: ");
+double d = Convert.ToDouble(Console.ReadLine());
+
+
+double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - Math.Pow(a, 2) / Math.Pow(b, 2);
+double y = (5 * (a + b) * (c - d)) / (0.5 * c) + Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a);
+double z = (Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
+double r = (0.5 * a + (3.0 / 4) * b - 7.0 / 5) / (3 * c + 1) + 1 / (a - c);
+
+
+Console.WriteLine($"x = {x}");
+Console.WriteLine($"y = {y}");
+Console.WriteLine($"z = {z}");
+Console.WriteLine($"r = {r}");
---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.Write("Введіть значення для a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення для b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення для m: ");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення для n: ");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть значення для x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+double z1 = (m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n) / Math.Sqrt(Math.Pow(m, 3) * n + n * m + Math.Pow(m, 2) - m);
+double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+double y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+
+Console.WriteLine($"z1 = {z1}");
+Console.WriteLine($"z2 = {z2}");
+Console.WriteLine($"y = {y}");
